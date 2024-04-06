a5ebfe5e74587ec12aadbcefb94483a4b1b8da80
jacopo-derrico <derricoj.design@gmail.com>
2024-04-06 16:25:39 +0200

    technology seed

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            'HTML',
+            'CSS',
+            'JS',
+            'Vue',
+            'PHP',
+            'Laravel',
+        ];
+
+        foreach ($technologies as $element) {
+            $new_technology = new Technology();
+            $new_technology->name = $element;
+            $new_technology->slug = Str::slug($new_technology->name);
+            $new_technology->save();
+        }
     }
 }
